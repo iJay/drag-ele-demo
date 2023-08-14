4386b713ed7beace78dba1b535ec4244c49423a1
hblx2 <izhanlixu@163.com>
2023-08-14 11:33:01 +0800

    docs: 指定本周开发功能

---
+++ src/components/common/ShapeWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="shape-wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: "ShapeWrapper",
+};
+</script>
+
+<style lang="sass">
+.shape-wrapper {}
+</style>

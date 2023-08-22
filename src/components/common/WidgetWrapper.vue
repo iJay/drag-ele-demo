b068acb2fab15a754df4ce977acf9a3392d7a46a
hblx2 <izhanlixu@163.com>
2023-08-22 16:49:18 +0800

    feat: 增加widget包裹组件的修改大小的功能

--- src/components/common/WidgetWrapper.vue
+++ src/components/common/WidgetWrapper.vue
@@ -4,6 +4,7 @@
     :class="[selected ? 'selected' : '']"
     @click="handleSelectedClick"
     :style="componentData.style"
+    ref="widgetWrapper"
     draggable="true"
     @dragstart="handleDragStart"
   >
@@ -29,6 +30,15 @@
         title="设置"
       ></i>
     </div>
+    <template v-if="selected">
+      <i
+        class="drag-dot"
+        :class="`drag-dot—${item}`"
+        v-for="(item, index) in 4"
+        :key="index"
+        @mousedown="(e) => handleStartResize(e, item)"
+      ></i>
+    </template>
     <component
       :is="componentData.componentName"
       :componentData="componentData"
@@ -38,11 +48,26 @@
 </template>
 
 <script>
-// import _ from "lodash";
+import _ from "lodash";
 import { mapActions, mapState } from "vuex";
 import { generateId } from "@/utils";
 export default {
   name: "WidgetWrapper",
+  data() {
+    return {
+      isResizing: false,
+      startX: 0,
+      startY: 0,
+      startHeight: 0,
+      startWidth: 0,
+      newHeight: 0,
+      newWidth: 0,
+      positionStartX: 0,
+      positionStartY: 0,
+      positionX: 0,
+      positionY: 0,
+    };
+  },
   props: {
     componentData: {
       type: Object,
@@ -77,50 +102,102 @@ export default {
       this.updateSelectedComponent(this.componentData.id);
     },
     handleDragStart(e) {
-      const widgetItem = this.componentData;
-      e.dataTransfer.effectAllowed = "copy";
-      e.dataTransfer.setData("text/plain", widgetItem.componentName);
-      let data;
-      if (!widgetItem.id) {
-        data = {
-          componentName: widgetItem.componentName,
-          id: generateId(),
-        };
-      } else {
-        data = {
-          componentName: widgetItem.componentName,
-          id: widgetItem.id,
-        };
+      if (!this.isResizing) {
+        const widgetItem = this.componentData;
+        e.dataTransfer.effectAllowed = "copy";
+        e.dataTransfer.setData("text/plain", widgetItem.componentName);
+        let data;
+        if (!widgetItem.id) {
+          data = {
+            componentName: widgetItem.componentName,
+            id: generateId(),
+          };
+        } else {
+          data = {
+            componentName: widgetItem.componentName,
+            id: widgetItem.id,
+          };
+        }
+        e.dataTransfer.setData("application/json", JSON.stringify(data));
       }
-      e.dataTransfer.setData("application/json", JSON.stringify(data));
     },
-    // handleStartResize(event) {
-    //   this.isResizing = true;
-    //   this.startY = event.clientY;
-    //   this.startHeight =
-    //     this.$refs.containerComponent.$el.getBoundingClientRect().height;
-
-    //   document.addEventListener("mousemove", _.throttle(this.resize, 100));
-    //   document.addEventListener("mouseup", this.stopResize);
-    // },
-    // resize(event) {
-    //   if (this.isResizing) {
-    //     const deltaY = event.clientY - this.startY;
-    //     this.newHeight = this.startHeight + deltaY;
-    //     this.$refs.containerComponent.$el.style.height = `${this.newHeight}px`;
-    //   }
-    // },
-    // stopResize(e) {
-    //   e.stopPropagation();
-    //   this.updateComponentAttr({
-    //     id: this.componentData.id,
-    //     attrKey: "height",
-    //     attrValue: `${this.newHeight}px`,
-    //   });
-    //   this.isResizing = false;
-    //   document.removeEventListener("mousemove", this.resize);
-    //   document.removeEventListener("mouseup", this.stopResize);
-    // },
+    handleStartResize(event, item) {
+      event.stopPropagation();
+      event.preventDefault();
+      this.dragDotIndex = item;
+      this.isResizing = true;
+      this.startY = event.clientY;
+      this.startX = event.clientX;
+      this.positionStartX = this.$refs.widgetWrapper.offsetLeft;
+      this.positionStartY = this.$refs.widgetWrapper.offsetTop;
+      console.log("this.positionStartX", this.positionStartX);
+      console.log("this.positionStartY", this.positionStartY);
+      this.startHeight =
+        this.$refs.wrapperComponent.$el.getBoundingClientRect().height;
+      this.startWidth =
+        this.$refs.wrapperComponent.$el.getBoundingClientRect().width;
+      document.addEventListener("mousemove", _.throttle(this.resize, 100));
+      document.addEventListener("mouseup", this.stopResize);
+    },
+    resize(event) {
+      if (this.isResizing) {
+        let deltaX, deltaY;
+        // TODO：待优化 硬编码
+        if ([2, 3].includes(this.dragDotIndex)) {
+          deltaX = event.clientX - this.startX;
+          if (this.dragDotIndex === 2) {
+            this.newWidth = this.startWidth - deltaX;
+            this.positionX = this.positionStartX + deltaX;
+            console.log("this.positionX", this.positionX);
+            this.$refs.widgetWrapper.style.left = `${this.positionX}px`;
+          } else {
+            this.newWidth = this.startWidth + deltaX;
+          }
+          this.$refs.wrapperComponent.$el.style.width = `${this.newWidth}px`;
+        } else {
+          deltaY = event.clientY - this.startY;
+          if (this.dragDotIndex === 1) {
+            this.newHeight = this.startHeight - deltaY;
+            this.positionY = this.positionStartY + deltaY;
+            console.log("this.positionY", this.positionY);
+            this.$refs.widgetWrapper.style.top = `${this.positionY}px`;
+          } else {
+            this.newHeight = this.startHeight + deltaY;
+          }
+          this.$refs.wrapperComponent.$el.style.height = `${this.newHeight}px`;
+        }
+      }
+    },
+    stopResize(e) {
+      e.stopPropagation();
+      // TODO: updateComponentAttr待优化 支持多个属性同时更新
+      this.updateComponentAttr({
+        id: this.componentData.id,
+        attrKey: "height",
+        attrValue: `${this.newHeight}px`,
+      });
+      this.updateComponentAttr({
+        id: this.componentData.id,
+        attrKey: "width",
+        attrValue: `${this.newWidth}px`,
+      });
+      if ([1, 2].includes(this.dragDotIndex)) {
+        console.log("this.updateComponentAttr");
+        this.updateComponentAttr({
+          id: this.componentData.id,
+          attrKey: "left",
+          attrValue: `${this.positionX}px`,
+        });
+        this.updateComponentAttr({
+          id: this.componentData.id,
+          attrKey: "top",
+          attrValue: `${this.positionY}px`,
+        });
+      }
+      this.isResizing = false;
+      document.removeEventListener("mousemove", this.resize);
+      document.removeEventListener("mouseup", this.stopResize);
+    },
     handleMoveToTop(e) {
       this.updateZIndexToTop({
         id: this.componentData.id,
@@ -162,6 +239,39 @@ export default {
 .widget-wrapper {
   position: relative;
   display: inline-block;
+
+  .drag-dot {
+    position: absolute;
+    width: 6px;
+    height: 6px;
+    border: 1px solid #409eff;
+    border-radius: 100%;
+    background: #fff;
+    &.drag-dot—1 {
+      top: -4px;
+      left: 50%;
+      transform: translateX(-50%);
+      cursor: n-resize;
+    }
+    &.drag-dot—2 {
+      top: 50%;
+      left: -4px;
+      transform: translateY(-50%);
+      cursor: w-resize;
+    }
+    &.drag-dot—3 {
+      top: 50%;
+      right: -4px;
+      transform: translateY(-50%);
+      cursor: e-resize;
+    }
+    &.drag-dot—4 {
+      bottom: -4px;
+      left: 50%;
+      transform: translateX(-50%);
+      cursor: s-resize;
+    }
+  }
 
   &.selected {
     border: 1px dashed #409eff;
